931f4b9c7deee39fd74d1a39a1aa35ad0ee09007
Cesar <cesar.bcnes@gmail.com>
2012-03-06 14:32:59 -0500

    Añadir controlador StaticPages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 SampleApp::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,11 +51,11 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  #root :to => 'static_pages#index'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
+end #End App
